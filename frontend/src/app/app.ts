60a9bbfc5137079fd828f5278d63d480d5f42514
Iulian Preda <iulian.preda.dev@protonmail.com>
2025-09-13 12:39:58 +0200

    Fixed setup for frontend

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HlmCardModule} from "@ui/card"
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HlmCardModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
